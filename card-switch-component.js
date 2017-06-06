88d5b91b37d224fa27b40e54cc600d0602c53301
Anwesh43 <anweshthecool0@gmail.com>
2017-06-06 14:10:21 +0530

    Adding Card

--- card-switch-component.js
+++ card-switch-component.js
@@ -2,22 +2,62 @@ const w = window.innerWidth,h = window.innerHeight
 class CardSwitchComponent extends HTMLElement {
     constructor() {
         super()
-        this.text = this.getAttribute('text')
+        const messages = this.getAttribute('messages')
         const shadow = this.attachShadow({mode:'open'})
-        this.img = document.createElement('img')
-        shadow.appendChild(this.img)
     }
     render() {
-        const canvas = document.createElement('canvas')
-        const context = canvas.getContext('2d')
-        this.img.src = canvas.toDataURL()
+
     }
     connectedCallback() {
         this.render()
     }
 }
 class Card {
-    constructor() {
-
+    constructor(message) {
+        this.img = document.createElement('img')
+        this.message = message
+        this.scale = 0
+        this.dir = 0
+        this.img.onmousedown = (event)=>{
+            if(this.scale == 0 && this.dir == 0) {
+                this.dir = 1
+            }
+        }
+    }
+    addToShadow(shadow) {
+        shadow.appendChild(this.img)
+    }
+    draw() {
+        const canvas = document.createElement('canvas')
+        canvas.width = Math.max(w,h)/10
+        canvas.height = Math.max(w,h)/10
+        const w = canvas.width
+        const context = canvas.getContext('2d')
+        context.fillStyle = '#2196F3'
+        context.fillRect(0,0,w,w)
+        context.save()
+        context.translate(w/2,w/2)
+        context.scale(this.scale,this.scale)
+        context.fillRect(-w/2,-w/2,w,w)
+        context.restore()
+        this.img.src = canvas.toDataURL()
+    }
+    setDir(dir) {
+        this.dir = dir
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    update() {
+        this.scale += this.dir * 0.2
+        if(this.scale > 1 || this.scale < 0) {
+            this.dir = 0
+            if(this.scale > 1) {
+                this.scale = 0
+            }
+            if(this.scale < 0) {
+                this.scale = 0
+            }
+        }
     }
 }
